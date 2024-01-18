cb64c130844a2a6cca8f6aa7c16ec3ff807e2a06
Rupinder Singh <techformation.rupinder@gmail.com>
2024-01-18 12:44:42 +0530

    Summary updated

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,15 +3,28 @@
 @extends('layouts.app')
 
 @section('content')
-<a href="{{ route('posts.add') }}">Add Post</a>
-<h2>All Posts</h2>
-<ul>
-    @forelse($posts as $post)
-    <li>
-        <a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a>
-    </li>
-    @empty
-    <p>No posts available.</p>
-    @endforelse
-</ul>
+<div class="container mt-3">
+    <a class="btn btn-primary mb-3" href="{{ route('posts.add') }}">Add Post</a>
+    <div class="card">
+        <div class="card-header bg-dark text-light">
+            <h2 class="m-0">All Posts</h2>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                @forelse($posts as $post)
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a>
+                    <form action="{{ route('posts.destroy', $post->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this post?')">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                    </form>
+                </li>
+                @empty
+                <li class="list-group-item">No posts available.</li>
+                @endforelse
+            </ul>
+        </div>
+    </div>
+</div>
 @endsection
