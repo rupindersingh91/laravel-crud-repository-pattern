52360bddd9c761353209e4db18eb42c9c8835952
Rupinder Singh <techformation.rupinder@gmail.com>
2024-01-18 12:46:33 +0530

    Show post UI updated

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,14 +3,24 @@
 @extends('layouts.app')
 
 @section('content')
-<a href="{{ route('posts.index') }}">All Posts</a>
+<div class="container mt-4">
+    <a href="{{ route('posts.index') }}" class="btn btn-secondary mb-3">All Posts</a>
 
-<h2>{{ $post->title }}</h2>
-<p>{{ $post->content }}</p>
-<a href="{{ route('posts.edit', $post->id) }}">Edit Post</a>
-<form action="{{ route('posts.destroy', $post->id) }}" method="POST">
-    @csrf
-    @method('DELETE')
-    <button type="submit" onclick="return confirm('Are you sure you want to delete this post?')">Delete Post</button>
-</form>
+    <div class="card">
+        <div class="card-header bg-primary text-white">
+            <h2>{{ $post->title }}</h2>
+        </div>
+        <div class="card-body">
+            <p class="card-text">{{ $post->content }}</p>
+        </div>
+        <div class="card-footer">
+            <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-warning">Edit Post</a>
+            <form action="{{ route('posts.destroy', $post->id) }}" method="POST" class="d-inline">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this post?')">Delete Post</button>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
