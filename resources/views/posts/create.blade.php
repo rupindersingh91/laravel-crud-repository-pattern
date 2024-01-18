1f5c8c1f53a191b1ae755909bbafdb2eca453a07
Rupinder Singh <techformation.rupinder@gmail.com>
2024-01-18 12:37:20 +0530

    UI updated for create form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,15 +3,17 @@
 @extends('layouts.app')
 
 @section('content')
-<h2>Create a New Post</h2>
-<form action="{{ route('posts.store') }}" method="POST">
-    @csrf
-    <label for="title">Title:</label>
-    <input type="text" name="title" required>
-    <br>
-    <label for="content">Content:</label>
-    <textarea name="content" rows="4" required></textarea>
-    <br>
-    <button type="submit">Create Post</button>
-</form>
+<div class="container">
+    <h2>Create a New Post</h2>
+    <form action="{{ route('posts.store') }}" method="POST" class="mb-3">
+        @csrf
+        <label class="form-label" for="title">Title:</label>
+        <input type="text" class="form-control" name="title" required>
+        <br>
+        <label class="form-label" for="content">Content:</label>
+        <textarea name="content" class="form-control" rows="4" required></textarea>
+        <br>
+        <button class="btn btn-primary" type="submit">Create Post</button>
+    </form>
+</div>
 @endsection
